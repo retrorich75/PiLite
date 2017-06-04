3cfe1b12c0f2dc342a1b778dcc2adc9ac2f070df
Richard Parslow <rmparslow@gmail.com>
2017-06-05 00:23:00 +0200

    Updated for API Key
    Openweather.org requires a registration to obtain a valid API key 
    Free plan is okay for up to 60 calls per minute http://openweathermap.org/appid

--- Python_Examples/PiLiteWeather.py
+++ Python_Examples/PiLiteWeather.py
@@ -7,7 +7,8 @@ from PiLiteLib import PiLiteBoard, poll_for_updates, JSONPoll
 class WeatherPoll(JSONPoll):
     def __init__(self, location, message_format=""):
         default_format = "{name}: {weather[0][description]}, {main[temp_c]:.0f}C"
-        base_url = "http://api.openweathermap.org/data/2.5/weather?q=%s"
+        # You must replace appid=xxxxxxxxxxxxxxxx with your own vaild key (free plan is okay) http://openweathermap.org/appid
+        base_url = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=xxxxxxxxxxxxxxxx"
         super(WeatherPoll, self).__init__(base_url%location,
                                           message_format or default_format)
 
