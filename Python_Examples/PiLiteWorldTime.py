752bf2f34d772db30c09b421bf341e0c4726bd55
Richard Parslow <rmparslow@gmail.com>
2017-06-04 19:27:52 +0200

    added comments
    Added comments on how to customise which timezones can be used and how to speed up the wait before showing next time

--- Python_Examples/PiLiteWorldTime.py
+++ Python_Examples/PiLiteWorldTime.py
@@ -15,12 +15,15 @@ class WorldTime(object):
 
         
 def main():
+    # You can customise which timezones to show by adding the appropriate City and Continent/City
+    # as can be found on the following WikiPedia page http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
     source = CyclingSources(WorldTime("London", "Europe/London"),
                WorldTime("Cairo", "Africa/Cairo"),
                WorldTime("Paris", "Europe/Paris"))
     sink = PiLiteBoard()
     print("ready")
     sink.write("ready  ")
+    # the number 60 before /len is the number of seconds to wait before showing the next time
     poll_for_updates(source, sink, 60/len(source))
 
 
